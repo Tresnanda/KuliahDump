de7691abc6a60d889888375cbf6c249cca278ee8
Treshnanda <112388754+Tresnanda@users.noreply.github.com>
2023-05-29 17:50:19 +0800

    Add files via upload
    nambah fitur sort

--- Strukdat/Double LinkedList.c
+++ Strukdat/Double LinkedList.c
@@ -1,65 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{
+struct Node {
     int data;
     struct Node* next;
     struct Node* prev;
 };
 
-struct List{
+struct List {
     struct Node* head;
 };
 
-void insert(struct List* llist, int data){
+void insert(struct List* llist, int data) {
     struct Node* new = (struct Node*)malloc(sizeof(struct Node));
     new->data = data;
     new->next = NULL;
     new->prev = NULL;
-    if(llist->head == NULL){
+    if (llist->head == NULL) {
         llist->head = new;
         return;
     }
-    if(llist->head->next == NULL){
+    if (llist->head->next == NULL) {
         llist->head->next = new;
         new->prev = llist->head;
         return;
     }
     struct Node* curr = llist->head;
-    while(curr->next != NULL){
+    while (curr->next != NULL) {
         curr = curr->next;
     }
     curr->next = new;
     new->prev = curr;
 }
 
-void hapus(struct List* list, int data){
-    if(list->head == NULL){
+void hapus(struct List* list, int data) {
+    if (list->head == NULL) {
         printf("Tidak ada data!");
         return;
     }
     struct Node* curr = list->head;
-    if(list->head->data == data){
-        if(list->head->next == NULL){
+    if (list->head->data == data) {
+        if (list->head->next == NULL) {
             list->head = NULL;
             printf("Data berhasil dihapus!");
             return;
-        }
-        else if(list->head->next != NULL){
+        } else if (list->head->next != NULL) {
             list->head = list->head->next;
             free(curr);
             printf("Data berhasil dihapus!");
             return;
         }
     }
-    while(curr!=NULL && curr->data != data){
+    while (curr != NULL && curr->data != data) {
         curr = curr->next;
     }
-    if(curr == NULL){
+    if (curr == NULL) {
         printf("Data tidak ditemukan!");
         return;
     }
-    if(curr->next == NULL){
+    if (curr->next == NULL) {
         curr->prev->next = NULL;
         free(curr);
         printf("Data berhasil dihapus!");
@@ -71,60 +70,93 @@ void hapus(struct List* list, int data){
     printf("Data berhasil dihapus!");
 }
 
-void display(struct List* list){
+void display(struct List* list) {
     struct Node* curr = list->head;
-    if(list->head == NULL){
+    if (list->head == NULL) {
         printf("Tidak ada data!");
         return;
     }
-    while(curr != NULL){
+    while (curr != NULL) {
         printf("%d ", curr->data);
         curr = curr->next;
     }
 }
 
-int main()
-{
-    struct List list = {NULL};
+void urut(struct List* list) {
+    if (list->head == NULL || list->head->next == NULL)
+        return;
+
+    int swapped;
+    struct Node* ptr1;
+    struct Node* lptr = NULL;
+
+    do {
+        swapped = 0;
+        ptr1 = list->head;
+
+        while (ptr1->next != lptr) {
+            if (ptr1->data > ptr1->next->data) {
+                int temp = ptr1->data;
+                ptr1->data = ptr1->next->data;
+                ptr1->next->data = temp;
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+}
+
+int main() {
+    struct List list = { NULL };
     int data;
     int choice;
     char tumbal[3];
-    do{
+    do {
         system("clear");
         printf("1. Insert data");
         printf("\n2. Delete data");
         printf("\n3. Display list");
+        printf("\n4. Urut data");
         printf("\n0. Exit\n");
         scanf("%d", &choice);
-        switch(choice){
-            case 1:
-                system("clear");
-                printf("Data yang ingin diinsert: ");
-                scanf("%d", &data);
-                insert(&list, data);
-                printf("Data berhasil diinsert!\n");
-                printf("\nTekan Enter untuk keluar ");
-                getchar();
-                fgets(tumbal, 3, stdin);
-                break;
-            case 2:
-                system("clear");
-                printf("Data yang ingin dihapus: ");
-                scanf("%d", &data);
-                hapus(&list, data);
-                printf("\nTekan Enter untuk keluar ");
-                getchar();
-                fgets(tumbal, 3, stdin);
-                break;
-            case 3:
-                system("clear");
-                display(&list);
-                printf("\nTekan Enter untuk keluar ");
-                getchar();
-                fgets(tumbal, 3, stdin);
-                break;
+        switch (choice) {
+        case 1:
+            system("clear");
+            printf("Data yang ingin diinsert: ");
+            scanf("%d", &data);
+            insert(&list, data);
+            printf("Data berhasil diinsert!\n");
+            printf("\nTekan Enter untuk keluar ");
+            getchar();
+            fgets(tumbal, 3, stdin);
+            break;
+        case 2:
+            system("clear");
+            printf("Data yang ingin dihapus: ");
+            scanf("%d", &data);
+            hapus(&list, data);
+            printf("\nTekan Enter untuk keluar ");
+            getchar();
+            fgets(tumbal, 3, stdin);
+            break;
+        case 3:
+            system("clear");
+            display(&list);
+            printf("\nTekan Enter untuk keluar ");
+            getchar();
+            fgets(tumbal, 3, stdin);
+            break;
+        case 4:
+            system("clear");
+            urut(&list);
+            printf("Data berhasil diurutkan secara ascending!\n");
+            printf("\nTekan Enter untuk keluar ");
+            getchar();
+            fgets(tumbal, 3, stdin);
+            break;
         }
-    }while(choice!=0);
+    } while (choice != 0);
 
     return 0;
 }
